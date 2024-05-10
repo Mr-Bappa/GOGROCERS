f4572bf915b92188e65473d01c86e07323c578a2
Mr-Bappa <bappadityamaity907@gmail.com>
2024-05-10 11:04:25 +0530

    Your descriptive commit message here

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,21 +1,23 @@
-/// Import the functions you need from the SDKs you need
+// Import Firebase libraries
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
-// Your web app's Firebase configuration
+import { getFirestore } from "firebase/firestore";
+
+// Your Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC7rul9AxNn7p9sbMwyJHLshXvSWEw3HLc",
   authDomain: "gogrocers-9552c-84429.firebaseapp.com",
   projectId: "gogrocers-9552c",
   storageBucket: "gogrocers-9552c.appspot.com",
   messagingSenderId: "157448340415",
-  appId: "1:157448340415:web:56bf8b6a2a3f74c4d6570a"
+  appId: "1:157448340415:web:56bf8b6a2a3f74c4d6570a",
 };
 
-// Initialize Firebase
+// Initialize Firebase and Firebase Auth
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); // Initialize Firebase Authentication
-export { app,auth };
+const auth = getAuth(app);
 
+// Initialize Firestore
+const db = getFirestore(app);
 
+export { app, auth, db };
